28731a3b5d9b9725173821fa3ec4f7b4768f1b18
TheReal13Random <ebrandon597@gmail.com>
2023-12-17 01:22:02 -0800

    BinaryTree

---
+++ Project 10/Project10.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "BinarySearchTreeLab10.h"
+#include "Random.h"
+using namespace std;
+
+int main()
+{
+	rand_seed();
+	BinarySearchTree<int> mybst;
+	int how_many;
+	cout << "How many random values? ";
+	cin >> how_many;
+	cout << endl << endl;
+
+
+	for (int i = 1; i <= how_many; i++)
+	{
+		mybst.insert(rand_int(1, 250));
+	}
+	cout << endl << how_many << " values entered" << endl << endl;
+
+	cout << "Here are the current values: " << endl;
+	mybst.printElements();
+	cout << endl << endl;
+
+	cout << "Here is the structure of the BST: " << endl;
+	mybst.printInternal();
+	cout << endl << endl;
+
+	cout << "Number of values contained in BST: " << mybst.size();
+	cout << endl << endl;
+	
+	return 0;
+}
